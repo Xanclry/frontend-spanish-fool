b8cb0041846cd326fd53923d61d8d89074f9ad38
Fiodar Rekish <fed_rekish@hotmail.com>
2021-04-12 23:30:44 +0300

    github pages fix

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,17 @@ import { history } from './config'
 import { Provider } from 'react-redux'
 import store from './state'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router history={history}>
-        <App />
-      </Router>
-    </Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
